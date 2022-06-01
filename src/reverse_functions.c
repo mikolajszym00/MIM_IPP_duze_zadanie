6d1625d402ae113d178e164b2243e18543c36556
Mikołaj <mikolaj.szym00@gmail.com>
2022-06-01 22:39:07 +0200

    checkBelow no comments

--- src/reverse_functions.c
+++ src/reverse_functions.c
@@ -60,71 +60,28 @@ void addForwardsFromPFToPhoneTrie(Trie trieOfForwards, Trie phoneTrie, Trie* for
 }
 
 bool checkBelow(Trie tr, Trie numberEnd) {
-//     printf("1 ");
-
     Trie* arr = tr->arrayOfTries;
-    if (arr == NULL || numberEnd->depth == tr->forwardCounter) {
-//        printf("2 ");
-        return true; }
+    if (arr == NULL || numberEnd->depth == tr->forwardCounter) { return true; }
 
-    char* restOfNum = calloc(numberEnd->depth - tr->forwardCounter + 1, sizeof(char)); // +- 1
-    if (restOfNum == NULL) {
-//        printf("3 ");
-        return true; } // troche nie prawda
-//    char* newNum = calloc(tr->forwardCounter+1, sizeof(char));
-
-//    getNumberFromTrie(tr, newNum); // , tr->depth-1// całkiem podobne do prepare phone trie
-
-//    printf("------------------\n");
-//    printf("number: %s\n", newNum);
-//
-//    printf("numend depth %zd\n", numberEnd->depth);
-//    printf("tr depth %zd\n", tr->forwardCounter);
-
-//    char* newnumberEnd = calloc(numberEnd->depth+1, sizeof(char));
-//
-//    getNumberFromTrie(numberEnd, newnumberEnd);
-//
-//    printf("---------aa--------\n");
-//    printf("number: %s\n", newnumberEnd);
-
+    char* restOfNum = calloc(numberEnd->depth - tr->forwardCounter + 1, sizeof(char));
+    if (restOfNum == NULL) { return true; }
 
     getNumberFromTrieWithStop(numberEnd, numberEnd->depth - tr->forwardCounter, restOfNum);
-//    printf("3.6 \n");
-//
-
-
-//    printf("rest number: %s\n", restOfNum);
-
-//    char* newNum = calloc(tr->depth+1, sizeof(char));
-//
-//    getNumberFromTrie(tr, newNum); // , tr->depth-1// całkiem podobne do prepare phone trie
-
-//    printf("number: %s\n", restOfNum);
-
-//
-//    printf("%c\n", restOfNum[0]);
 
     int restOfNumValue;
     getInteger(&restOfNumValue, restOfNum[0]);
-//    printf("asddd");
-//
-//    printf("restof numb %d\n", restOfNumValue);
-//    if (arr[3] != NULL) {printf("nie maaa!\n"); }
-//    printf("------------------\n");
+
     for (int i=0; i<=restOfNumValue; i++) {
         if (arr[i] != NULL) {
             Trie newEnd = addNumber(tr, restOfNum);
-            newEnd->forwardCounter = numberEnd->depth; // +- 1
+            newEnd->forwardCounter = numberEnd->depth;
             tr->forwardCounter = 0;
 
-//            printf("4 ");
             free(restOfNum);
             return false;
         }
     }
 
-//    printf("5 ");
     free(restOfNum);
     return true;
 }
